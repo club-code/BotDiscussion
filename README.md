43eedae80e249169cbaf79ebf8e50b0e5108fc25
Ardhanaariishvara <5360806-Ardhanaariishvara@users.noreply.gitlab.com>
2020-07-26 00:50:17 +0200

    Architecture for the project

---
+++ README.md
@@ -0,0 +1,5 @@
+# DiscuBot
+*name will probably change*
+
+A Discord bot that aims to provide a lean way of referencing the subject of a discussion, its start, the different steps and arguments provided by each side, and a conclusion, as well as sources.
+
--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,16 +1,75 @@
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.optional
+import com.github.ajalt.clikt.parameters.options.option
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-fun main(args: Array<String>) {
-    JDABuilder
-        .createDefault(args[0])
-        .addEventListeners(Bot())
-        .build()
-}
+//fun main(args: Array<String>) {
+//    JDABuilder
+//        .createDefault(args[0])
+//        .addEventListeners(Bot())
+//        .build()
+//}
 
 class Bot : ListenerAdapter() {
     override fun onMessageReceived(event: MessageReceivedEvent) {
 
     }
 }
+
+class Start: CliktCommand(help="Initializes a debate") {
+    val name by argument()
+    val msgStart : String? by argument(help = "Start of the message").optional()
+
+    override fun run() {
+        echo("Hi! Name is $name and start is $msgStart")
+    }
+}
+
+class End: CliktCommand(help = "Ends a debate") {
+    val name by argument()
+    val msgStart : String? by argument().optional()
+
+    override fun run() {
+
+    }
+}
+
+class Argument: CliktCommand() {
+    val name by argument()
+    val person by argument()
+    val title by argument()
+    val text by argument()
+
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
+
+class Modify:CliktCommand() {
+    val name by argument()
+    val id by argument() //think about the way to call the argument
+    val text by argument()
+
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
+
+class List:CliktCommand() {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
+
+class Display:CliktCommand() {
+    val id by argument()
+
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main(args:Array<String>) = Start().main(args)
